8799645e6e5f50c2183009ed811889a9c318dcf2
TejoPtv <134220546+TejoPtv@users.noreply.github.com>
2023-06-08 20:47:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "16995961"))
+API_HASH = environ.get("API_HASH", "8817a7d4293049593e60999359970ddd")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6139769072:AAEpgstIHthfZ60Yx0vwq9X50ozAyz6SVf4")
+ADMIN = int(environ.get("ADMIN", "1640783367"))          
+CAPTION = environ.get("CAPTION", "nothing boy")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
